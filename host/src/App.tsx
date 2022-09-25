3b4a172674c58c0a455edbad3d0a3965306ee648
Jesus Martin <jesus.martin@froged.com>
2022-09-25 23:08:37 +0200

    Add base project with host

---
+++ host/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+const App = () => (
+  <div>
+    Hello World
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
