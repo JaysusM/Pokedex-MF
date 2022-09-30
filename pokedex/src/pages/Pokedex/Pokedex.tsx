62d89c16a07ae3183376cd207065ad4f2b2a4a86
Jesus Martin <jesus.martin@froged.com>
2022-09-30 21:55:36 +0200

    Add Search input

--- pokedex/src/pages/Pokedex/Pokedex.tsx
+++ pokedex/src/pages/Pokedex/Pokedex.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Input from '../../components/Input/Input';
 import Menu from '../../components/Menu/Menu';
 import './Pokedex.scss';
 
 const Pokedex = () => {
 
-    return <>
+    return <div className="pokedex-app-wrapper">
+        <Input />
         <Menu />
-    </>;
+    </div>;
 }
 
 export default Pokedex;
