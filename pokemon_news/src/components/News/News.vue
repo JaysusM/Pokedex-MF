1eaf111c74f84733c719f4ccab6fce35b6cc0fc1
Jesus Martin <jesus.martin@froged.com>
2022-10-27 23:47:21 +0200

    Little refactor in News and add News skeleton

--- pokemon_news/src/components/News/News.vue
+++ pokemon_news/src/components/News/News.vue
@@ -4,13 +4,8 @@
             <h3>Pokémon News</h3>
             <a @click="this.onViewAll?.()">View all</a>
         </div>
-        <div v-for="item in news" class="news-item" v-on:click="openNewsItem(item)">
-            <div class="news-item-title">
-                <h4 v-html="item.title"></h4>
-                <p>{{ item.date }}</p>
-            </div>
-            <img v-bind:src=item.image />
-        </div>
+        <NewsItem v-if="!loading" v-for="item in news" :newsItem="item" />
+        <NewsItem v-if="loading" v-for="_ in 3" :skeleton="true" />
     </div>
 </template>
 
@@ -30,62 +25,38 @@
     .news-wrapper > .news-header > h3 {
         margin: 0;
     }
-
-    .news-wrapper > .news-item {
-        display: flex;
-        flex-direction: row;
-        max-height: 130px;
-        background: #00000024;
-        padding: 0 0 0 10px;
-        border-radius: 5px;
-        margin: 10px 0;
-        cursor: pointer;
-    }
-
-    .news-wrapper > .news-item > .news-item-title {
-        display: flex;
-        flex-direction: column;
-        min-width: calc(60% - 25px);
-        padding-right: 5px;
-    }
-    
-    .news-wrapper > .news-item > .news-item-title > h4 {
-        margin-bottom: 0;
-    }
-
-    .news-wrapper > .news-item > img {
-        min-width: 40%;
-        object-fit: cover;
-        flex: 1;
-        border-radius: 30px;
-        padding: 15px 10px;
-    }
 </style>
 
 <script>
     import NewsApi from '../../shared/news-api';
+    import NewsItem from '../NewsItem/NewsItem.vue';
     export default {
-        name: "news",
-        props: {
-            onViewAll: { type: Function }
-        },
-        methods: {
-            openNewsItem: (item) => {
-                window.open(item.url, '_blank');
-            }
-        },
-        data() {
-            return {
-                loading: true,
-                news: []
-            }
-        },
-        created() {
-            return NewsApi.getLatestNews()
-                .then((news) => {
-                    this.loading = false;
-                    this.news = news;
-                });
+    name: "news",
+    props: {
+        onViewAll: { type: Function }
+    },
+    components: [
+        NewsItem
+    ],
+    methods: {
+        openNewsItem: (item) => {
+            window.open(item.url, "_blank");
         }
-    }
+    },
+    data() {
+        return {
+            loading: true,
+            news: []
+        };
+    },
+    created() {
+        return NewsApi.getLatestNews()
+            .then((news) => {
+                this.loading = false;
+                this.news = news;
+            }
+        );
+    },
+    components: { NewsItem }
+}
 </script>
---
+++ pokemon_news/src/components/NewsItem/NewsItem.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="news-item" :class="skeleton && 'skeleton'" v-on:click="openNewsItem(newsItem)">
+        <div class="news-item-title">
+            <h4 v-if="!skeleton" v-html="newsItem.title"></h4>
+            <p v-if="!skeleton">{{ newsItem.date }}</p>
+        </div>
+        <img v-if="!skeleton" v-bind:src=newsItem.image />  
+    </div>
+</template>
+
+<style scoped>
+    .news-item {
+        display: flex;
+        flex-direction: row;
+        max-height: 130px;
+        min-height: 130px;
+        background: #00000024;
+        padding: 0 0 0 10px;
+        border-radius: 5px;
+        margin: 10px 0;
+        cursor: pointer;
+    }
+
+    @keyframes waveEffect {
+        from {
+            left: -100%;
+        }
+
+        50% {
+            left: 50%;
+        }
+
+        to {
+            left: 200%;
+        }
+    }
+
+
+    .news-item.skeleton {
+        position: relative;
+        overflow: hidden;
+        cursor: default;
+    }
+
+    .news-item.skeleton::after { 
+        content: '';
+        width: 100%;
+        left: -100%;
+        height: 100%;
+        position: absolute;
+        animation: waveEffect 2.5s linear infinite;
+        background: linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.1), transparent);
+    }
+
+    .news-item > .news-item-title {
+        display: flex;
+        flex-direction: column;
+        min-width: calc(60% - 25px);
+        padding-right: 5px;
+    }
+    
+    .news-item > .news-item-title > h4 {
+        margin-bottom: 0;
+    }
+
+    .news-item > img {
+        min-width: 40%;
+        object-fit: cover;
+        flex: 1;
+        border-radius: 30px;
+        padding: 15px 10px;
+    }
+</style>
+
+<script>
+    export default {
+        name: "news-item",
+        props: {
+            skeleton: Boolean,
+            newsItem: Object
+        },
+        methods: {
+            openNewsItem: (item) => {
+                if (this.skeleton)
+                    return;
+                window.open(item.url, '_blank');
+            }
+        }
+    }
+</script>
