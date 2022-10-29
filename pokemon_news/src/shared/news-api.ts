1537c94e1108826a7f7bd1a5965a71fd33c473c3
Jesus Martin <jesus.martin@froged.com>
2022-10-30 01:47:37 +0200

    Change news service

--- pokemon_news/src/shared/news-api.ts
+++ pokemon_news/src/shared/news-api.ts
@@ -7,16 +7,17 @@ class NewsApi {
         // @ts-ignore
         const apiKey = process.env.NEWS_API_KEY;
 
-        return fetch('https://newsapi.org/v2/everything?q=pokemon&sortBy=publishedAt&apiKey=' + apiKey, { method: 'GET' })
+        return fetch('https://newsdata.io/api/1/news?q=pokemon&country=us&language=en&category=entertainment,technology,top&apikey=' + apiKey, { method: 'GET' })
             .then((response: Response) => response.json())
-            .then(({ articles }: { articles: any[]}) => {
-                return articles
+            .then(({ results }: { results: any[]}) => {
+                return results
+                    .filter(article => article.image_url)
                     .slice(0, 10)
                     .map(article => ({
                         title: article.title,
-                        image: article.urlToImage,
-                        url: article.url,
-                        date: moment(article.publishedAt).format('DD MMM YYYY')
+                        image: article.image_url,
+                        url: article.link,
+                        date: moment(article.pubDate).format('DD MMM YYYY')
                     }))
             });
     }
