9ecc2a3120e3770f49d4edc386ba4f267ce2701e
Jesus Martin <jesus.martin@froged.com>
2022-10-30 21:19:19 +0100

    Change publicPath for news and pokedex

--- pokemon_news/webpack.config.js
+++ pokemon_news/webpack.config.js
@@ -3,61 +3,63 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const { VueLoaderPlugin } = require("vue-loader");
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  output: {
-    publicPath: argv.mode === 'development' ? "http://localhost:4001/" : 'https://pokedexmf-news.vercel.app/',
-  },
+module.exports = (_, argv) => {
+  return {
+    output: {
+      publicPath: argv.mode === 'development' ? "http://localhost:4001/" : 'https://pokedexmf-news.vercel.app/',
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".vue", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 4001,
-    historyApiFallback: true
-  },
+    devServer: {
+      port: 4001,
+      historyApiFallback: true
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.tsx?$/,
-        use: [
-          "babel-loader",
-          {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
-              appendTsSuffixTo: ["\\.vue$"],
-              happyPackMode: true,
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test: /\.tsx?$/,
+          use: [
+            "babel-loader",
+            {
+              loader: "ts-loader",
+              options: {
+                transpileOnly: true,
+                appendTsSuffixTo: ["\\.vue$"],
+                happyPackMode: true,
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-    ],
-  },
+          ],
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+      ],
+    },
 
-  plugins: [
-    new Dotenv(),
-    new VueLoaderPlugin(),
-    new ModuleFederationPlugin({
-      name: "pokemon_news",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        './News': './src/components/News/News.ts'
-      },
-      shared: require("./package.json").dependencies,
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+    plugins: [
+      new Dotenv(),
+      new VueLoaderPlugin(),
+      new ModuleFederationPlugin({
+        name: "pokemon_news",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          './News': './src/components/News/News.ts'
+        },
+        shared: require("./package.json").dependencies,
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+  }
 };
